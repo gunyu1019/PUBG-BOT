b814e3165517d6debf104b399c43cee69ce06668
gunyu1019 <gunyu1019@gmail.com>
2020-06-15 16:11:42 +0900

    상세 전적 기능 작업완료

--- src/start_n.py
+++ src/start_n.py
@@ -561,6 +561,108 @@ async def season_status_update(player_id,season,message):
     except:
         connect.close()
     return return_value
+async def profile_mode_status(message,pubg_platform,pubg_type,mode,pubg_json,season,player_id):
+    embed = discord.Embed(color=0xffd619,timestamp=datetime.datetime.now(timezone('UTC')))
+    embed.set_author(icon_url="attachment://" + image_name[pubg_platform] ,name=await player_name(player_id) + "님의 전적")
+    json_c = pubg_json["data"]["attributes"]["gameModeStats"][mode]
+    assists = json_c["assists"]
+    boosts = json_c["boosts"]
+    dBNOs = json_c["dBNOs"]
+    dailyKills = json_c["dailyKills"]
+    dailyWins = json_c["dailyWins"]
+    damageDealt = json_c["damageDealt"]
+    days = json_c["days"]
+    headshotKills = json_c["headshotKills"]
+    heals = json_c["heals"]
+    kills = json_c["kills"]
+    longestKill = json_c["longestKill"]
+    longestTimeSurvived = json_c["longestTimeSurvived"]
+    losses = json_c["losses"]
+    maxKillStreaks = json_c["maxKillStreaks"]
+    mostSurvivalTime = json_c["mostSurvivalTime"]
+    revives = json_c["revives"]
+    rideDistance = json_c["rideDistance"]
+    roadKills = json_c["roadKills"]
+    roundMostKills = json_c["roundMostKills"]
+    roundsPlayed = json_c["roundsPlayed"]
+    suicides = json_c["suicides"]
+    swimDistance = json_c["swimDistance"]
+    teamKills = json_c["teamKills"]
+    timeSurvived = json_c["timeSurvived"]
+    top10s = json_c["top10s"]
+    vehicleDestroys = json_c["vehicleDestroys"]
+    walkDistance = json_c["walkDistance"]
+    weaponsAcquired = json_c["weaponsAcquired"]
+    weeklyKills = json_c["weeklyKills"]
+    weeklyWins = json_c["weeklyWins"]
+    wins = json_c["wins"]
+    season_count = season.replace("division.bro.official.pc-2018","")
+    if int(season_count) < 7:
+        rank_title, rank_icon = ranking(json_c["rankPointsTitle"],0)
+        embed.set_thumbnail(url="attachment://" + rank_icon.replace("assets" + type_software + "Ranks" + type_software,""))
+        icon = discord.File(directory + type_software + rank_icon)
+    if int(wins) + int(top10s) + int(losses) == 0:
+        winper = "0"
+    else:
+        winper = str(int(wins) / (int(wins) + int(top10s) + int(losses)) * 100)
+    if int(kills) != 0:
+        headshot_per = str(int(headshotKills) / int(kills) * 100)
+    else:
+        headshot_per = "0"
+    if int(losses) != 0:
+        KD = str(round(int(kills) / int(losses),2))
+    else:
+        KD = str(round(int(kills) / 1,2))
+    playtime1 = datetime.datetime.fromtimestamp(float(timeSurvived),timezone('UTC'))
+    playtime2 = datetime.datetime.fromtimestamp(float(longestTimeSurvived),timezone('UTC'))
+    playtime3 = datetime.datetime.fromtimestamp(float(mostSurvivalTime),timezone('UTC'))
+    a_playtime = time_num(playtime1)
+    max_playtime = time_num(playtime2)
+    average_playtime = time_num(playtime3)
+    def distance(distance):
+        return str(round(float(distance)/1000,2))
+    data1 = "게임 수:" + str(int(wins) + int(top10s) + int(losses)) + "회\n승리:" + str(wins) + "회\nTop10:" + str(top10s) + "회\n패배:" + str(losses) + "회\n승률:" + winper + "%\n일간 승리:" + str(dailyWins) + "회\n주간 승리:" + str(weeklyWins) + "회"
+    data2 = "일간 킬 수" + str(dailyKills) + "회\n주간 킬 수:" + str(weeklyKills) + "회\n게임당 최다 킬 수:" +  str(maxKillStreaks) + "회\n누적 킬 수:" +  str(kills) + "회\n헤드샷:" +  str(headshotKills) + "회(" + headshot_per + "%)\n어시스트:" +  str(assists) + "회\nK/D:" +  KD + "\n차량 파괴:" +  str(vehicleDestroys) + "회\n로드킬:" +  str(roadKills) + "회\n최장 킬 거리:" +  str(round(float(longestKill),2)) + "m\n자살:" +  str(suicides) + "회\n팀킬:" +  str(teamKills) + "회"
+    data3 = "플레이 시간:" + a_playtime + "\n이번 시즌 게임 접속일:" + str(days) + "일\n최대 생존 시간:" + max_playtime + "\n평균 생존 시간:" + average_playtime + "\n치유:" + str(heals) + "회\n부스트:" + str(boosts) + "회"
+    data4 = "종합 이동 거리:" + str(round(float(distance(walkDistance)) + float(distance(rideDistance)) + float(distance(swimDistance)),2)) + "km\n걸어간 거리:" + distance(walkDistance) + "km\n탑승 거리:" + distance(rideDistance) + "km\n평균 생존 시간:" + distance(swimDistance) + "km"
+    data5 = "누적 입힌 피해:" + str(round(float(damageDealt),2)) + "\n무기 획득 횟수:" + str(weaponsAcquired) + "회\nDBNO:" + str(dBNOs) + "회\n소생:" + str(revives) + "회"
+    embed.add_field(name="플레이 기록:",value=data1,inline=False)
+    embed.add_field(name="전투 기록:",value=data2,inline=False)
+    embed.add_field(name="게임 플레이:",value=data3,inline=False)
+    embed.add_field(name="이동 거리:",value=data4,inline=False)
+    embed.add_field(name="기타:",value=data5,inline=False)
+    if int(season_count) < 7:
+        icons = [image(pubg_platform),icon]
+        msg1 = await message.channel.send(files=icons,embed=embed)
+    else:
+        msg1 = await message.channel.send(file=image(pubg_platform),embed=embed)
+    for i in range(3):
+        await msg1.add_reaction(str(i+1) + "\U0000FE0F\U000020E3")
+    msg2 = await message.channel.send("\U00000031\U0000FE0F\U000020E3 : 개요 \U00000032\U0000FE0F\U000020E3 : 전적 정보 업데이트 \U00000033\U0000FE0F\U000020E3 : 메뉴중지")
+    author = message.author
+    message_id = msg1.id
+    def check(reaction, user):
+        for i in range(3):
+            if str(i+1) + "\U0000FE0F\U000020E3" == reaction.emoji:
+                return user == author and message_id == reaction.message.id
+    reaction,user = await client.wait_for('reaction_add', check=check)
+    if reaction.emoji == "\U00000031\U0000FE0F\U000020E3":
+        await msg1.delete()
+        await msg2.delete()
+        await profile_mode(message,pubg_platform,pubg_type,mode,pubg_json,season,player_id)
+        #await profile_mode(message,pubg_platform,pubg_type,mode,update_json,season,player_id)
+    elif reaction.emoji == "\U00000032\U0000FE0F\U000020E3":
+        await msg1.delete()
+        await msg2.delete()
+        update_json = await season_status_update(player_id,season,message)
+        if update_json == "Failed_Response":
+            return
+        await profile_mode_status(message,pubg_platform,pubg_type,mode,update_json,season,player_id)
+        return
+    elif reaction.emoji == "\U00000033\U0000FE0F\U000020E3":
+        await msg1.clear_reactions()
+        await msg2.delete()
+        return
 
 async def profile_mode(message,pubg_platform,pubg_type,mode,pubg_json,season,player_id):
     list_message = message.content.split(" ")
@@ -627,6 +729,7 @@ async def profile_mode(message,pubg_platform,pubg_type,mode,pubg_json,season,pla
     if reaction.emoji == "\U00000031\U0000FE0F\U000020E3":
         await msg1.delete()
         await msg2.delete()
+        await profile_mode_status(message,pubg_platform,pubg_type,mode,pubg_json,season,player_id)
         return
     elif reaction.emoji == "\U00000032\U0000FE0F\U000020E3":
         await msg1.delete()
@@ -639,7 +742,7 @@ async def profile_mode(message,pubg_platform,pubg_type,mode,pubg_json,season,pla
     elif reaction.emoji == "\U00000033\U0000FE0F\U000020E3":
         await msg1.delete()
         await msg2.delete()
-        await profile_total(message,pubg_platform,pubg_type,update_json,season,player_id)
+        await profile_total(message,pubg_platform,pubg_type,pubg_json,season,player_id)
         return
     elif reaction.emoji == "\U00000034\U0000FE0F\U000020E3":
         await msg1.clear_reactions()
