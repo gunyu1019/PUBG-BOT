c705619cbb2a2f214b5e9e9a88f1be43c1f03ced
gunyu1019 <gunyu1019@gmail.com>
2021-08-05 02:39:08 +0900

    Logging process

--- cogs/event.py
+++ cogs/event.py
@@ -9,10 +9,8 @@ from module.interaction import SlashContext, Message
 from utils import token
 
 logger = logging.getLogger(__name__)
-
-
-def log_system(msg):
-    logger.info(msg)
+logger_command = logging.getLogger(__name__ + ".command")
+logger_guild = logging.getLogger(__name__ + ".guild")
 
 
 class Events(commands.Cog):
@@ -21,10 +19,10 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        log_system(f'디스코드 봇 로그인이 완료되었습니다.')
-        log_system(f"디스코드봇 이름: {self.bot.user.name}")
-        log_system(f"디스코드봇 ID: {str(self.bot.user.id)}")
-        log_system(f"디스코드봇 버전: {discord.__version__}")
+        logger.info(f'디스코드 봇 로그인이 완료되었습니다.')
+        logger.info(f"디스코드봇 이름: {self.bot.user.name}")
+        logger.info(f"디스코드봇 ID: {str(self.bot.user.id)}")
+        logger.info(f"디스코드봇 버전: {discord.__version__}")
         print('------------')
         answer = ""
 
@@ -34,9 +32,9 @@ class Events(commands.Cog):
                 self.bot.guilds[i].id) + "):" + str(
                 self.bot.guilds[i].member_count) + "명\n"
             total += self.bot.guilds[i].member_count
-        log_system(f"방목록: \n{answer}\n방의 종합 멤버:{total}명")
+        logger.info(f"방목록: \n{answer}\n방의 종합 멤버:{total}명")
 
-        log_system(f'DBSkr이 실행됩니다.')
+        logger.info(f'DBSkr이 실행됩니다.')
         dbs = logging.getLogger("DBSkr")
         dbs.setLevel(logging.DEBUG)
         DBSkr.Client(
@@ -54,21 +52,21 @@ class Events(commands.Cog):
             if i.name == guild.name:
                 server_number = self.bot.guilds.index(i) + 1
         if server_number is not None:
-            log_system(
+            logger_guild.info(
                 guild.name + '에 가입이 확인되었습니다. 서버번호: ' + str(server_number) + '번, 서버멤버' + str(guild.member_count) + '명')
         return
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        log_system(guild.name + '로 부터 추방 혹은 차단되었습니다.')
+        logger_guild.info(guild.name + '로 부터 추방 혹은 차단되었습니다.')
         return
 
     @commands.Cog.listener()
     async def on_command(self, ctx: Union[SlashContext, Message]):
         if ctx.guild is not None:
-            logger.info(f"({ctx.guild} | {ctx.channel} | {ctx.author}) {ctx.content}")
+            logger_command.info(f"({ctx.guild} | {ctx.channel} | {ctx.author}) {ctx.content}")
         else:
-            logger.info(f"(DM채널 | {ctx.author}) {ctx.content}")
+            logger_command.info(f"(DM채널 | {ctx.author}) {ctx.content}")
 
 
 def setup(client):
--- config/log_config.py
+++ config/log_config.py
@@ -2,16 +2,31 @@ import logging
 
 from utils.directory import directory
 
-log = logging.getLogger()
-
-log.setLevel(logging.INFO)
-
 logging_format = logging.Formatter('[%(asctime)s | %(name)s | %(levelname)s]: %(message)s', "%Y-%m-%d %p %I:%M:%S")
-logging_file = logging.FileHandler(f'{directory}/log/log.txt', mode='a', encoding='UTF8')
-logging_file.setFormatter(logging_format)
 
+
+def get_logger(name: str = None):
+    _log = logging.getLogger(name)
+    _log.setLevel(logging.INFO)
+    return _log
+
+
+log = get_logger()
 command = logging.StreamHandler()
 command.setFormatter(logging_format)
-
 log.addHandler(command)
-log.addHandler(logging_file)
+
+log_event = get_logger("cogs.event.command")
+logging_file = logging.FileHandler(f'{directory}/log/command.txt', mode='a', encoding='UTF8')
+logging_file.setFormatter(logging_format)
+log_event.addHandler(logging_file)
+
+log_event = get_logger("cogs.event.guild")
+logging_file = logging.FileHandler(f'{directory}/log/event.txt', mode='a', encoding='UTF8')
+logging_file.setFormatter(logging_format)
+log_event.addHandler(logging_file)
+
+log_error = get_logger("cogs.error")
+logging_file = logging.FileHandler(f'{directory}/log/error.txt', mode='a', encoding='UTF8')
+logging_file.setFormatter(logging_format)
+log_error.addHandler(logging_file)
